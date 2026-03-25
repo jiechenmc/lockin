2326b6054c1c20115bbdccf18e8e382420606022
Jie Chen <jiechenmc@gmail.com>
2026-03-24 21:33:15 -0500

    show hh/mm

--- frontend/src/ui/table.tsx
+++ frontend/src/ui/table.tsx
@@ -53,6 +53,9 @@ const RecordTable = () => {
                   month: "2-digit",
                   day: "2-digit",
                   year: "2-digit",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  hour12: false,
                 })}
               </td>
               <td>{record.Ack ? "✅" : "❌"}</td>
